5d58a01c2e3e6b03824ffcd396a51b609d7ca1f2
Vishwas <vishwas.g5@gmail.com>
2020-11-19 21:46:57 +0530

    Binding Classes

--- vue-fundamentals/src/App.vue
+++ vue-fundamentals/src/App.vue
@@ -1,25 +1,53 @@
 <template>
+  <!-- Binding Text -->
   <div>{{ greet }} {{ name }}</div>
+
+  <!-- Binding HTML -->
   <div v-html="channel"></div>
   <div v-html="hack"></div>
+
+  <!-- Bidning Attributes -->
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+
+  <!-- Binding Classes -->
+  <h2 class="underline">Underlined Text</h2>
+  <h2 v-bind:class="status">Status</h2>
+  <h2 class="underline" v-bind:class="status">Static Dynamic</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? Movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
+    Array Conditional Movie
+  </h2>
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut,
+    }"
+  >
+    Object Conditional Movie
+  </h2>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
   data() {
     return {
-      name: "Vishwas",
-      greet: "Hello",
-      channel: "<b>Codevolution</b>",
+      name: 'Vishwas',
+      greet: 'Hello',
+      channel: '<b>Codevolution</b>',
       hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!<a/>`,
-      headingId: "heading",
+      headingId: 'heading',
       isDisabled: true,
-    };
+      status: 'danger',
+      isPromoted: false,
+      isSoldOut: true,
+    }
   },
-};
+}
 </script>
 
 <style>
@@ -30,5 +58,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: olivedrab;
+}
+.sold-out {
+  color: red;
 }
 </style>
