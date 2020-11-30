548f5193d240402f9499d71ddde5adef6dccd6db
Vishwas <vishwas.g5@gmail.com>
2020-11-30 19:24:11 +0530

    Conditional rendering with template

--- vue-fundamentals/src/App.vue
+++ vue-fundamentals/src/App.vue
@@ -72,6 +72,9 @@
     <h2>{{ name }}</h2>
     <hr />
   </template>
+  <template v-for="name in names" :key="name">
+    <h2 v-if="name === 'Bruce'">{{ name }}</h2>
+  </template>
 </template>
 
 <script>
