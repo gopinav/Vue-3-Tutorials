9ffdb887bcade3d81e7f7ba063520d52464ba3d2
Vishwas <vishwas.g5@gmail.com>
2020-12-08 12:27:45 +0530

    Modifiers

--- vue-fundamentals/src/App.vue
+++ vue-fundamentals/src/App.vue
@@ -105,10 +105,10 @@
       {{ JSON.stringify(formValues, null, 2) }}
     </pre>
   </div>
-  <form @submit="submitForm">
+  <form @submit.prevent="submitForm">
     <div>
       <label for="name">Name</label>
-      <input type="text" id="name" v-model="formValues.name" />
+      <input type="text" id="name" v-model.trim="formValues.name" />
     </div>
     <div>
       <label for="profile">Profile Summary</label>
@@ -197,8 +197,17 @@
       <label for="10+">10+</label>
     </div>
     <div>
-      <button>Submit</button>
+      <label for="age">Age</label>
+      <input
+        type="number"
+        id="age"
+        v-model.number="formValues.age"
+        @keyup.enter="submitForm"
+      />
     </div>
+    <!-- <div>
+      <button>Submit</button>
+    </div> -->
   </form>
 </template>
 
@@ -271,6 +280,7 @@ export default {
         remoteWork: 'no',
         skillSet: [],
         yearsOfExperience: '',
+        age: null,
       },
     }
   },
@@ -292,8 +302,7 @@ export default {
       this.name = 'Batman'
       console.log('Event', event)
     },
-    submitForm(event) {
-      event.preventDefault()
+    submitForm() {
       console.log('form values', this.formValues)
     },
   },
