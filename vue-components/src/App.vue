48cc56e013fe0d4df429d7491e365a8cc6f03214
Vishwas <vishwas.g5@gmail.com>
2020-12-28 20:27:14 +0530

    Greet component

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Greet />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Greet from './components/Greet.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Greet,
+  },
 }
 </script>
 
---
+++ vue-components/src/components/Greet.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Hello Vishwas</h2>
+</template>
+
+<script>
+export default {
+  name: 'Greet',
+}
+</script>
+
+<style scoped>
+</style>
