86e07901608a76fc941356aff720fda92342e9d2
Vishwas <vishwas.g5@gmail.com>
2021-01-02 20:19:29 +0530

    Custom Events

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,18 +1,26 @@
 <template>
+  <!-- Props -->
   <Greet :name="name" :hero-name="channel" />
   <Greet name="Bruce" hero-name="Batman" />
   <Greet name="Clark" hero-name="Superman" />
   <Greet name="Diana" hero-name="Wonder Woman" />
 
   <Article id="my-article" :likes="50" :isPublished="true" />
+
+  <!-- Provide/ Inject -->
   <h3>App component {{ name }}</h3>
   <ComponentC />
+
+  <!-- Events -->
+  <button @click="showPopup = true">Show Popup</button>
+  <Popup v-show="showPopup" @close="showPopup = false" />
 </template>
 
 <script>
 import Article from './components/Article.vue'
 import Greet from './components/Greet.vue'
 import ComponentC from './components/ComponentC.vue'
+import Popup from './components/Popup.vue'
 
 export default {
   name: 'App',
@@ -20,11 +28,13 @@ export default {
     Greet,
     Article,
     ComponentC,
+    Popup,
   },
   data() {
     return {
       name: 'Vishwas',
       channel: 'Codevolution',
+      showPopup: false,
     }
   },
   provide() {
---
+++ vue-components/src/components/Popup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>This is a popup</h2>
+    <button @click="$emit('close')">Close Popup</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popup',
+  emits: ['close'],
+}
+</script>
+
+<style scoped>
+</style>
