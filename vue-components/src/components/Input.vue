6d827117514e67d31d7f654e6c2be2c7a4a0f99c
Vishwas <vishwas.g5@gmail.com>
2021-01-04 11:51:49 +0530

    v-model on components

---
+++ vue-components/src/components/Input.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    modelValue: String,
+  },
+  emits: ['update:modelValue'],
+}
+</script>
+
+<style scoped>
+input[type='text'] {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.4;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
