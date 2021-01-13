80097124232fb5b5750e1861febc9391beb48d7b
Vishwas <vishwas.g5@gmail.com>
2021-01-13 15:09:20 +0530

    Lifecycle Hooks

---
+++ vue-http/src/components/Child.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>Child component</div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  beforeCreate() {
+    console.log('Child beforeCreate()')
+  },
+  created() {
+    console.log('Child created()')
+  },
+  beforeMount() {
+    console.log('Child beforeMount()')
+  },
+  mounted() {
+    console.log('Child mounted()')
+  },
+  beforeUpdate() {
+    console.log('Child beforeUpdate()')
+  },
+  updated() {
+    console.log('Child updated()')
+  },
+  beforeUnmount() {
+    console.log('Child beforeUnmount()')
+  },
+  unmounted() {
+    console.log('Child unmounted()')
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue-http/src/components/Parent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    Parent component
+    <button @click="showChild = !showChild">Toggle Child</button>
+    <Child v-if="showChild" />
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+  components: { Child },
+  name: 'Parent',
+  data() {
+    return {
+      showChild: true,
+    }
+  },
+  beforeCreate() {
+    console.log('Parent beforeCreate()')
+  },
+  created() {
+    console.log('Parent created()')
+  },
+  beforeMount() {
+    console.log('Parent beforeMount()')
+  },
+  mounted() {
+    console.log('Parent mounted()')
+  },
+  beforeUpdate() {
+    console.log('Parent beforeUpdate()')
+  },
+  updated() {
+    console.log('Parent updated()')
+  },
+  beforeUnmount() {
+    console.log('Parent beforeUnmount()')
+  },
+  unmounted() {
+    console.log('Parent unmounted()')
+  },
+}
+</script>
+
+<style scoped>
+</style>
