5e7e985b7de244e32c90f1a307fc862d8104e29d
Vishwas <vishwas.g5@gmail.com>
2021-01-18 19:20:57 +0530

    Remove HelloWorld

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,16 +1,9 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
 }
 </script>
 
