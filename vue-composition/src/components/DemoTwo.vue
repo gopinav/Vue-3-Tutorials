ec56feff19610e953bb2342766407c498da9b493
Vishwas <vishwas.g5@gmail.com>
2021-01-20 19:53:17 +0530

    Understanding toRefs

---
+++ vue-composition/src/components/DemoTwo.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>{{ firstName }} {{ lastName }}</h2>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+export default {
+  name: 'DemoTwo',
+  setup() {
+    const state = reactive({
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+    })
+
+    setTimeout(() => {
+      state.firstName = 'Clark'
+      state.lastName = 'Kent'
+      console.log(state)
+    }, 2000)
+    return toRefs(state)
+  },
+}
+</script>
+
+<style scoped>
+</style>
