a1b789a7eaf89fb230802661dc9cd9648d410598
Vishwas <vishwas.g5@gmail.com>
2021-01-20 19:53:04 +0530

    Understanding Reactivity

---
+++ vue-composition/src/components/DemoOne.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>{{ name }}</h2>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'DemoOne',
+  setup() {
+    const name = ref('Vishwas')
+
+    setTimeout(() => {
+      name.value = 'Codevolution'
+    }, 2000)
+    return {
+      name,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
