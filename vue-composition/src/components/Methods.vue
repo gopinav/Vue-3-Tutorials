43bdd8c6e373190885ba084521bf6f40b20768ee
Vishwas <vishwas.g5@gmail.com>
2021-01-21 18:30:56 +0530

    Composition API methods

---
+++ vue-composition/src/components/Methods.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- Options API -->
+  <h2>{{ name }}</h2>
+  <button @click="setName">Set Name</button>
+
+  <!-- ref -->
+  <h2>{{ count }}</h2>
+  <button @click="incrementCount">Increment Count</button>
+
+  <!-- reactive -->
+  <h2>{{ first }} {{ last }}</h2>
+  <button @click="changeHero">Change Hero</button>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from 'vue'
+export default {
+  name: 'Methods',
+  setup() {
+    const count = ref(0)
+    function incrementCount() {
+      count.value++
+    }
+
+    const state = reactive({
+      first: 'Bruce',
+      last: 'Wayne',
+    })
+    function changeHero() {
+      state.first = 'Clark'
+      state.last = 'Kent'
+    }
+
+    return {
+      count,
+      incrementCount,
+      ...toRefs(state),
+      changeHero,
+    }
+  },
+  data() {
+    return {
+      name: 'Vishwas',
+    }
+  },
+  methods: {
+    setName() {
+      this.name = 'Codevolution'
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
