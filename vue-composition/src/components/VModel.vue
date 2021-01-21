7ecc0a4761c231bf2886fa8bf59c8d616e692f91
Vishwas <vishwas.g5@gmail.com>
2021-01-21 19:33:18 +0530

    v-model and composition API

---
+++ vue-composition/src/components/VModel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- Options API -->
+    <input type="text" placeholder="First Name" v-model="fName" />
+    <input type="text" placeholder="Last Name" v-model="lName" />
+
+    <!-- ref -->
+    <input type="text" placeholder="Hero Name" v-model="heroName" />
+
+    <!-- reactive -->
+    <input type="text" placeholder="First Name" v-model="firstName" />
+    <input type="text" placeholder="Last Name" v-model="lastName" />
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from 'vue'
+export default {
+  name: 'VModel',
+  setup() {
+    const heroName = ref('')
+    const state = reactive({
+      firstName: '',
+      lastName: '',
+    })
+
+    return {
+      heroName,
+      ...toRefs(state),
+    }
+  },
+  data() {
+    return {
+      fName: '',
+      lName: '',
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
