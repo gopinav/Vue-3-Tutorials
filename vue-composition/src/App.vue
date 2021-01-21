06ad62d38daeccf7e6ad8768cc2839bc1566c39c
Vishwas <vishwas.g5@gmail.com>
2021-01-21 07:45:03 +0530

    Reactivity and toRefs

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <Data />
+  <DemoOne />
+  <DemoTwo />
 </template>
 
 <script>
 import Data from './components/Data.vue'
+import DemoOne from './components/DemoOne.vue'
+import DemoTwo from './components/DemoTwo.vue'
 
 export default {
   name: 'App',
   components: {
     Data,
+    DemoOne,
+    DemoTwo,
   },
 }
 </script>
