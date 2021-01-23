ddf091f11902002f250cd98a79f8e136495eae29
Vishwas <vishwas.g5@gmail.com>
2021-01-23 20:28:58 +0530

    Template Refs

---
+++ vue-composition/src/components/TemplateRef.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" ref="inputRef" />
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+export default {
+  name: 'TemplateRef',
+  setup() {
+    const inputRef = ref(null)
+
+    onMounted(() => {
+      inputRef.value.focus()
+    })
+
+    return {
+      inputRef,
+    }
+  },
+  //   mounted() {
+  //     this.$refs.inputRef.focus()
+  //   },
+}
+</script>
+
+<style scoped>
+</style>
